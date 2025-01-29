f4c554417f121e5598f946a384cc90c7e0a1ca5c
Joydev2000 <137753258+Joydev2000@users.noreply.github.com>
2025-01-29 22:59:01 +0530

    cc

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import Header from "./Layout/Header";
+
 const App = () => {
   return (
     <>
-      <h1>hello</h1>
+      <div className="main w-full bg-black">
+        <Header />
+      </div>
     </>
   );
 };
-
 export default App;
---
+++ src/Layout/Header.jsx
@@ -0,0 +1,16 @@
+const Header = ()=>{
+    return(
+        <div className="Navbar w-full text-white px-20 py-5 flex justify-between">
+      <div className="logo">
+      <h2 className="text-2xl font-bold">Joydev.</h2>
+      </div>
+      <div className="links flex gap-10">
+      {["Services", "Our Work", "About Us", "insight","Contact Us"].map ((items, index) => {
+          return <a key={index} href="" className={`text-2xl font-['Neue Montreal'] ${index === 4 && "ml-60"}`}>{items}</a>
+      })}
+      </div>
+    </div>
+    )
+}
+
+export default Header;
